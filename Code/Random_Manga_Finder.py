ad82c67f196c9aeb7649f0df3f4067a7e30d7d14
nolannish <nolannish@gmail.com>
2024-07-15 15:46:34 -0700

    Created initial script for accessing MAL API
    Initial script accessing MAL API data, the main idea is to get the id, title, rank, and author of the individual manga, and then list them in the output. For the future, potentially creating a website that will display more information

---
+++ Code/Random_Manga_Finder.py
@@ -0,0 +1,17 @@
+import requests
+import random
+import json
+
+BASE_URL = 'https://api.jikan.moe/v4/'
+RAND_INT = random.randint(1, 1000)
+response = requests.get(f'https://api.jikan.moe/v4/manga/{RAND_INT}')
+
+response_dict = response.json()
+
+y = json.dumps(response_dict, indent=4, sort_keys=True)
+x = json.loads(y)
+
+print(x["data"]["mal_id"])
+print(x["data"]["title"])
+print(x["data"]["rank"])
+print(x["data"]["authors"][0]["name"])
